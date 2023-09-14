7c2efa492dc88a4d270c5b170eb5014fe990ab91
etienu <89298915+etienu@users.noreply.github.com>
2023-09-14 17:08:32 +0900

    コード確認用

--- src/assets/plugin/YesNoChart/YesNoChart_JQ.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    jQuery('.yn-chart a').click(function() {
-        console.log(" : yesno_chart : load");
-        jQuery(this).closest('div').css('display', 'none');
-        id = jQuery(this).attr('href');
-        jQuery(id).fadeIn('fast');
-        return false;
-    })
-};

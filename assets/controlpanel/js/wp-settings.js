00c0f18614d0d6ba0127e9b40e2faff56e325188
etienu <89298915+etienu@users.noreply.github.com>
2023-10-31 17:13:05 +0900

    Add files via upload

--- assets/controlpanel/js/wp-settings.js
+++ assets/controlpanel/js/wp-settings.js
@@ -6,7 +6,9 @@
 //----------------------------------------------------------------
 //  読み込み時の処理
 document.addEventListener("DOMContentLoaded",()=>{
-    const wpdir = "../wp-content/themes/eec/assets/controlpanel";
+    // テーマ名固定に注意( テーマより更に上の位置からはじまっているため )
+    const wpdir = "../wp-content/themes/blanktheme/assets/controlpanel";
+//    const wpdir = "../wp-content/themes/eec/assets/controlpanel";
 
     //----------------------------------------------------
     //  管理画面にあるidを取得して、なければ管理画面ではないので終了
@@ -23,19 +25,27 @@ document.addEventListener("DOMContentLoaded",()=>{
     //----------------------------------------
     // ボタンの処理
     //----------------------------------------
+    //  記事リセット
     const btnms_vr = document.querySelector("#my-wp-setting-viewreset");
     if( btnms_vr )
         btnms_vr.addEventListener("click", () => {
             //  「ファイル」の配置位置
             //  管理画面の話なのでテーマより上にあった方がよいかもしれない
             //  しかし、wp-migration一発で終わらせたい事を考えるとテーマ下で管理したいとも思う
-            runPHP( wpdir+"/php/wp-setting-viewreset.php" );
+            runPHP( wpdir+"/php/wp-setting-viewreset.php", "custompost" );
+        });
+
+    //  アイテムカウンター : リセットボタン
+    const btnms_itemcounter = document.querySelector("#my-wp-setting-itemcounterreset");
+    if( btnms_itemcounter )
+        btnms_itemcounter.addEventListener("click", () => {
+            runPHP( wpdir+"/php/wp-setting-viewreset.php", "itemcounter" );
         });
 
     //----------------------------------------
     //  PHPの実行
     //----------------------------------------
-    const runPHP = ( i_fileName ) => {
+    const runPHP = ( i_fileName, i_id ) => {
         console.log( ">>> runPHP <<<" );
         // (1)XMLHttpRequestオブジェクトを作成
         var xhr = new XMLHttpRequest();
@@ -45,16 +55,19 @@ document.addEventListener("DOMContentLoaded",()=>{
             if(this.readyState == 4 && this.status == 200){
                 console.log( this.responseText );
             }
-            //console.log("[status]" + status);
-            //console.log(data);
-            //console.log('ファイル['+i_fileName+']読み込み成功');
+            if(this.readyState == 4 && this.status == 500){
+                console.log( this.responseText );
+            }
             //  リロードする
-            location.reload();
+            location.reload();            
         }
-        // (3)HTTPのGETメソッドとアクセスする場所を指定
-        xhr.open('GET', i_fileName, true );
+        // (3)HTTPのPOSTメソッドとアクセスする場所を指定
+        xhr.open('POST', i_fileName, true );
+        // (3)送信する内容の形式を設定
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        let send_txt = 'id=' + i_id;
         // (4)HTTPリクエストを送信
-        xhr.send();
+        xhr.send( send_txt );
     }
 
 });
